31b04febfdd0f915320aa8df563df3e698662776
mayurm.virtueinfo@gmail.com <mayurm.virtueinfo@gmail.com>
2025-06-11 20:18:05 +0530

    - ios firebase setup run

--- ios/PanditApp/AppDelegate.mm
+++ ios/PanditApp/AppDelegate.mm
@@ -9,7 +9,7 @@
 {
   
   self.moduleName = @"PanditApp";
-  
+  [FIRApp configure];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
