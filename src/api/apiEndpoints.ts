89a1f7e8cfa8a2f6c2aca46a69a080b8722a4d5c
sunny-virtueinfo <sunny19.virtueinfo@gmail.com>
2025-09-08 11:08:06 +0530

    Change UI for language switcher

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -32,8 +32,8 @@ class ApiEndpoints {
 
 export default ApiEndpoints;
 
-// export const BASE_URL = 'https://puja-guru.com';
-export const BASE_URL = 'https://4606b23f513e.ngrok-free.app';
+export const BASE_URL = 'https://puja-guru.com';
+// export const BASE_URL = 'https://e49fb80b9633.ngrok-free.app';
 
 export const POST_SIGNIN = '/app/auth/signin/';
 export const GET_CITY = '/app/areas/';
